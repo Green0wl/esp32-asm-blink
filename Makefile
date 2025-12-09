35a4537eaf102111916f3b1010546305f59f0d5b
Green0wl <kolomietsivan88@gmail.com>
2025-12-09 15:15:40 +0100

    docs: explain code

--- Makefile
+++ Makefile
@@ -1,9 +1,50 @@
+# The principles and syntax of Makefile are not explained here. Please refer to
+# other sources to understand how Makefile works.
+
+# When using '--text-section-literals' to place literals inline in the section
+# being assembled, the .literal_position directive can be used to mark a
+# potential location for a literal pool.
+#
+# The assembler will automatically place text section literal pools before
+# ENTRY instructions, so the .literal_position directive is only needed to
+# specify some other location for a literal pool.
+#
+# Literal pools are placed by default in separate literal sections; however
+# when using the '--text-section-literals' option, the literal pools for
+# PC-relative mode L32R instructions are placed in the current section.
+#
+# The .text section is explicitly placed at the beginning of SRAM0 by the
+# Linker Script. L32R instructions are restricted in that all literals must be
+# placed before use. Therefore, literals are inlined into the .text section
+# before they are used.
+# See: https://stackoverflow.com/a/26610227/17798036
+#
+# For more info, see:
+# https://sourceware.org/binutils/docs/as/Literal-Position-Directive.html
+# https://sourceware.org/binutils/docs/as/Xtensa-Options.html
 so.o: so.s
 	xtensa-esp32-elf-as --text-section-literals -o so.o so.s
 
+# -T is used to specify a linker script used. A Linker script's principal use
+# is specifying the format and layout of the final executable binary.
+#
+# ".elf" is Executable and Linkable Format.
+# See: https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
 so.elf: so.o so.ld
 	xtensa-esp32-elf-ld -T so.ld -o so.elf so.o
 
+# The elf2image command converts an ELF file (from compiler/linker output) into
+# the binary executable images which can be flashed and then booted into.
+# See:
+# https://docs.espressif.com/projects/esptool/en/latest/esp32/esptool/basic-commands.html
+#
+# Flash mode specifies how many GPIOs are used for SPI flash communication. SPI
+# flash memory is a type of non-volatile memory that uses a serial peripheral
+# interface (SPI) to communicate with a host processor.
+# See (not all devices support all modes):
+# https://docs.espressif.com/projects/esptool/en/latest/esp32/esptool/flash-modes.html
+#
+# '--flash_size' specifies how big flash region is.
 so.bin: so.elf
 	esptool.py --chip esp32 elf2image \
 		--flash_mode dio --flash_freq 40m --flash_size 4MB \
@@ -11,6 +52,16 @@ so.bin: so.elf
 
 all: so.bin
 
+# The next arguments to write_flash are one or more pairs of offset (address)
+# and file name.
+#
+# The first 0x1000 bytes of flash are reserved on ESP32 for Secure Boot. 2nd
+# stage bootloader is located at the 0x1000 address in flash memory, directly
+# after Secure Boot bytes. This application does not have any bootloader, so
+# so.bin is flashed in the place of the bootloader to be directly executed.
+#
+# See:
+# https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/partition-tables.html
 flash: all
 	esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 \
 			write_flash 0x1000 so.bin
--- so.s
+++ so.s
@@ -1,22 +1,97 @@
 /* so.s - ESP32 (ESP32-WROOM-32E module and ESP-32S board)
- * bare-metal LED blink on GPIO2. */
+ * bare-metal LED blink on GPIO2.
+ *
+ * Thanks to the guys who wrote
+ * http://cholla.mmto.org/esp32/bootrom/lightning.html
+ * and http://cholla.mmto.org/esp8266/xtensa.html.
+ * Most of the explanations were taken from there.
+ */
 
 .section        .text
 
+// Addresses below 0x4000_0000 are serviced using the data bus.  Addresses in
+// the range 0x4000_0000 ~ 0x4FFF_FFFF are serviced using the instruction bus.
+//
+// The data bus and instruction bus are both little-endian: for example, byte
+// addresses 0x0, 0x1, 0x2, 0x3 access the least significant, second least
+// significant, second most significant, and the most significant bytes of the
+// 32-bit word stored at the 0x0 address, respectively.
+// See: https://en.wikipedia.org/wiki/Endianness
+// See (page 66, "3.3.1 Address Mapping"):
+// https://documentation.espressif.com/esp32_technical_reference_manual_en.pdf
+//
+// .literal_position directive has logical meaning during compilation. See Makefile.
+// See: https://sourceware.org/binutils/docs-2.15/as/Literal-Position-Directive.html
 .literal_position
+// All docs for registers were taken from "ESP32 Technical Reference Manual Version 5.6".
+
+// These registers are located in the physical memory of the ESP32 and are
+// sensitive to the data stored in them. If the data in these registers
+// changes, the physical behavior of the corresponding GPIO also changes.
+
+// Register 6.8. GPIO_ENABLE_W1TS_REG (0x0024)
+// GPIO_ENABLE_W1TS_REG: GPIO0-31 output enable set register. For every bit
+// that is 1 in the value written here, the corresponding bit in GPIO_ENABLE
+// will be set. (WO)
 gpio_enable:    .word 0x3FF44024
+
+// Register 6.2. GPIO_OUT_W1TS_REG (0x0008)
+// GPIO_OUT_W1TS_REG GPIO0-31 output set register. For every bit that is 1 in
+// the value written here, the corresponding bit in GPIO_OUT_REG will be set.
+// (WO)
 gpio_out_set:   .word 0x3FF44008
+
+// Register 6.3. GPIO_OUT_W1TC_REG (0x000c)
+// GPIO_OUT_W1TC_REG GPIO0-31 output clear register. For every bit that is 1 in
+// the value written here, the corresponding bit in GPIO_OUT_REG will be
+// cleared. (WO)
 gpio_out_clr:   .word 0x3FF4400C
-gpio2_mask:     .word 0x00000004
+
+// This masks GPIO2 bit in registers above (**second** index from end).
+// See (a diagram explains it better than words):
+// https://documentation.espressif.com/esp32_technical_reference_manual_en.pdf#Regfloat.6.3
+gpio2_mask:     .word 0b00000100
+
+// Just a counter to slow down CPU and make a delay between LED activity.
 delay_count:    .word 2000000
 
 .global         call_start_cpu0
 
+// There are 16 tegisters named a0 through a15.
+//   a0 is special - it holds the call return address.
+//   a1 is used by gcc as a stack pointer.
+//   a2 gets used to pass a single argument (and to return a function value).
+
+// 1. Think of the load instructions as "flowing" from right to left. In other words,
+//    the first register is the destination, the remaining registers and/or stuff are
+//    operands.
+// 2. Store instructions are just the opposite, the first register is the
+//    source and will be stored at some address generated from the operands.
+// 3. Instructions like "add", "and", "xor" flow like the load. The first register is
+//    the destination and the operands are on the right.
+
 call_start_cpu0:
-    /* Enable GPIO2 as output */
+    /* Enable GPIO2 as output. The l32r instruction fetches a 32 bit constant
+     * that is stored (dumped) someplace nearby (hence the actual address where
+     * the constant is dumped is of little real interest). */
+    // This two instructions load data FROM constants TO registers in CPU.
+
+    // The Xtensa uses an instruction known as L32R to load constant values
+    // from memory that don't fit into the Xtensa's MOVI immediate load
+    // instruction, which has a 12-bit signed immediate field.
+    // See: https://stackoverflow.com/a/26610227/17798036
     l32r        a2, gpio_enable
     l32r        a3, gpio2_mask
+    /* memw is "memory wait". It is basically a CPU pipeline sync. It waits
+     * until all data loads and stores finish. */
     memw
+    // s32i(Ax, Ay, imm): [Ay + imm] = Ax ; Store a 32 bit word.
+    // imm is an offset used for pointer arithmetic: Ay[imm] = Ax.
+    // This instruction writes data FROM register (a3) TO data bus (a2[0]).
+    //
+    // This is needed to enable GPIO2, writing in GPIO_ENABLE_W1TS_REG the
+    // corresponding value (third less significant bit of the mask loaded in a3
+    // CPU register represents GPIO2).
     s32i        a3, a2, 0
     memw
 
@@ -27,24 +102,27 @@ blink_loop:
     memw
     s32i        a3, a2, 0
     memw
-    
+
     /* Delay */
     l32r        a4, delay_count
 delay1:
+    // a4 = a4 - 1
     addi        a4, a4, -1
+    // branch (i.e. jump to delay1) if not equal zero
     bnez        a4, delay1
-    
+
     /* Set GPIO2 LOW */
     l32r        a2, gpio_out_clr
     l32r        a3, gpio2_mask
     memw
     s32i        a3, a2, 0
     memw
-    
+
     /* Delay */
     l32r        a4, delay_count
 delay2:
     addi        a4, a4, -1
     bnez        a4, delay2
-    
+
+    // jump unconditionally; aka. blink_loop()
     j           blink_loop
